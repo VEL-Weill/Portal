434e525be55feb673210f19013f669f0a6d4f553
VEL-Weill <72039252+VEL-Weill@users.noreply.github.com>
2020-09-28 13:37:38 -0400

    Add files via upload

---
+++ WeillWebsitePortal/Js/Interface.js
@@ -0,0 +1,35 @@
+function opennn(){
+    window.open("https://redcap.ctsc.weill.cornell.edu/redcap_protocols/surveys/?s=K7FCANHJRN")
+  }
+  function Start(){
+  var code = document.getElementById('Code').value;
+  var MusicCode = "MM"
+  var WritingCode = "NW"
+  var mor = code[0]+code[1]
+  if(MusicCode == mor.toUpperCase()){
+  window.open("../html/VEL-Music.html")
+  }
+  else{
+  if(WritingCode == mor.toUpperCase()){
+  window.open("../html/VEL-Writing.html")
+  }
+  else{
+    alert("Invalid code")
+  }
+  }
+  }
+  function LaunchAbout(){
+    alert("launch about page")
+  }
+  function OpenTab(functionName) {
+if(functionName == 1){
+location.replace("../html/VEL-About.html")
+}
+if (functionName == 2){
+  location.replace(href = "../html/VEL-Email.html")
+}
+
+if (functionName == 3){
+  location.replace("../html/VEL-Main.html")
+}
+  }
---
+++ WeillWebsitePortal/Js/Platform.js
@@ -0,0 +1,4 @@
+if(navigator.platform === "MacIntel"){
+    document.getElementById('results').innerHTML = "If you accidently close any tab press command shift t";
+    document.getElementById('description').innerHTML = "Thank you for participating in the study, A Brief Phased Two-Step Intervention for Treating General Psychological Distress, PTSD, and Co-Morbidities in Healthcare Workers Consequent to the COVID-19 Pandemic.";
+    }
